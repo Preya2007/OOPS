1d5f268dfe427bd37f0ecaf1d9159154c617e206
Preya2007 <preya060307@gmail.com>
2026-01-11 18:09:46 +0530

    Remove enrollment number and clean up code

--- Practical_4.java
+++ Practical_4.java
@@ -4,16 +4,14 @@ public class Practical_4{
 	public static void main(String[] args){
 	
 		Scanner sc = new Scanner(System.in);
-		System.out.println("\nEnrollment No.: 240390107032");
+		
 		System.out.println("Enter Your Weigth In Pounds: ");
 		double w = sc.nextDouble();
-		
 		System.out.println("Enter Your Height in Inches: ");
 		double h = sc.nextDouble();
 		
 		double kg= w * 0.45359237;
 		System.out.println("Your Weight in Kilograms: "+ kg+ " kgs.");
-		
 		double meter= h * 0.0254;
 		System.out.println("Your Height in Meters: "+ meter+ " meter.");
 		
@@ -23,5 +21,6 @@ public class Practical_4{
 		sc.close();
 		
 	}
+
 
 }
