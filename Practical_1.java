496893061a5f2675f561b1ef4f5b18d3a8c533b4
Preya2007 <preya060307@gmail.com>
2026-01-11 18:06:18 +0530

    Update Practical_1.java

--- Practical_1.java
+++ Practical_1.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Practical_1{
 	public static void main(String[] args){
-		
-		System.out.println("Anrollment No. 240390107032");
 		Scanner sc = new Scanner(System.in);
 		System.out.print(" Enter Distance:");
 		double Distance = sc.nextDouble();
@@ -12,5 +10,6 @@ public class Practical_1{
 	sc.close();
 	
 }
+
 
 }
