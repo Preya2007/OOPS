f30bcb4303f1e7606fe2375809c49135eaa755fd
Preya2007 <preya060307@gmail.com>
2026-01-11 18:01:36 +0530

    Add files via upload

---
+++ Practical_5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Practical_5{
+	public static void main(String[] args){
+		System.out.println("Enrollment No. 240390107032");	
+
+		Scanner sc = new Scanner(System.in);
+		int amount =787;
+
+		System.out.printf("Number of 100's note: %d \n",amount / 100);
+		amount = amount%100;
+		System.out.printf("Number of 50's note: %d \n",amount/50);
+		amount = amount%50;
+		System.out.printf("Number of 10's note: %d \n",amount/10);
+		amount = amount%10;
+		System.out.printf("Number of 5's note: %d \n",amount/5);
+		amount = amount%5;
+		System.out.printf("Number of 2's note: %d \n",amount/2);
+		amount = amount%2;
+		System.out.printf("Number of 1's note: %d \n",amount);
+
+		sc.close();
+	}
+}
