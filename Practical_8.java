16a33d915047f0666bb10045653b480a2dd0828c
Preya2007 <preya060307@gmail.com>
2026-01-13 17:31:07 +0530

    Add files via upload

---
+++ Practical_8.java
@@ -0,0 +1,18 @@
+public class Practical_8 {
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Please run the program like this:");
+            System.out.println("java Practical_8 10 20");
+            return;
+        }
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
+
