f188b0cd9c753ea20a79f0fc058d984cf3b45a78
Preya2007 <preya060307@gmail.com>
2026-04-25 18:30:24 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,99 +1,51 @@
-import java.util.Scanner;
+class TablePrinter {
 
-// Interface
-interface Order {
-    void placeOrder(String item, int qty);
-    void cancelOrder(int orderId);
-    void generateBill();
-}
-
-// Abstract class with partial implementation
-abstract class PartialOrder implements Order {
-
-    String itemName;
-    int quantity;
-    int orderId;
-    static int counter = 1;
-
-    // Implement only placeOrder()
-    public void placeOrder(String item, int qty) {
-        this.itemName = item;
-        this.quantity = qty;
-        this.orderId = counter++;
-
-        System.out.println("Order Placed Successfully!");
-        System.out.println("Order ID: " + orderId);
-        System.out.println("Item: " + itemName);
-        System.out.println("Quantity: " + quantity);
-    }
-}
-
-// Concrete class implementing remaining methods
-class FinalOrder extends PartialOrder {
-
-    // Cancel order
-    public void cancelOrder(int id) {
-        if (this.orderId == id) {
-            System.out.println("Order ID " + id + " has been cancelled.");
-            itemName = null;
-            quantity = 0;
-        } else {
-            System.out.println("Invalid Order ID!");
+    synchronized void printTable(int num) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
         }
-    }
-
-    // Generate bill
-    public void generateBill() {
-        if (itemName == null) {
-            System.out.println("No active order to generate bill.");
-            return;
-        }
-
-        int pricePerItem = 100; // fixed price
-        int total = quantity * pricePerItem;
-
-        System.out.println("\n----- BILL -----");
-        System.out.println("Order ID: " + orderId);
-        System.out.println("Item: " + itemName);
-        System.out.println("Quantity: " + quantity);
-        System.out.println("Price per item: " + pricePerItem);
-        System.out.println("Total Amount: " + total);
+        System.out.println();
     }
 }
 
-// Main class
+class Thread1 extends Thread {
+    TablePrinter tp;
+
+    Thread1(TablePrinter tp) {
+        this.tp = tp;
+    }
+
+    public void run() {
+        tp.printTable(5);
+    }
+}
+
+class Thread2 extends Thread {
+    TablePrinter tp;
+
+    Thread2(TablePrinter tp) {
+        this.tp = tp;
+    }
+
+    public void run() {
+        tp.printTable(7);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+	System.out.println("Enrollment No. 240390107032");
+        TablePrinter tp = new TablePrinter();
 
-        Scanner sc = new Scanner(System.in);
-        FinalOrder order = new FinalOrder();
+        Thread1 t1 = new Thread1(tp);
+        Thread2 t2 = new Thread2(tp);
 
-        // Take input
-        System.out.print("Enter item name: ");
-        String item = sc.nextLine();
-
-        System.out.print("Enter quantity: ");
-        int qty = sc.nextInt();
-
-        // Place order
-        order.placeOrder(item, qty);
-
-        // User choice
-        System.out.println("\n1. Generate Bill");
-        System.out.println("2. Cancel Order");
-        System.out.print("Enter your choice: ");
-        int choice = sc.nextInt();
-
-        if (choice == 1) {
-            order.generateBill();
-        } else if (choice == 2) {
-            System.out.print("Enter Order ID to cancel: ");
-            int id = sc.nextInt();
-            order.cancelOrder(id);
-        } else {
-            System.out.println("Invalid choice!");
-        }
-
-        sc.close();
+        t1.start();
+        t2.start();
     }
 }
